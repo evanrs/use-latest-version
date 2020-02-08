6737f5ea57c7e61189e2e99cedcec170609d399f
evanrs <me@evanrs.com>
2020-02-08 15:26:24 -0500

    feat: add use-latest-version

---
+++ src/use-latest-version.ts
@@ -0,0 +1,37 @@
+import { ulid } from "ulid";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  DependencyList
+} from "react";
+
+type Value<T> = {
+  version: string;
+  value: T;
+};
+
+export function useLatestVersion<T>(
+  defaultValue: T,
+  dependencies: DependencyList
+): [Value<T>, (v: T) => void] {
+  const [current, setCurrent] = useState<Value<T>>({
+    version: "",
+    value: defaultValue
+  });
+  const [next, setNext] = useState<Value<T>>();
+
+  const version = useMemo(ulid, dependencies);
+  const update = useCallback((value: T) => setNext({ version, value }), [
+    version
+  ]);
+
+  useEffect(() => {
+    if (next && current.version < next.version) {
+      setCurrent(next);
+    }
+  }, [current, next]);
+
+  return [current, update];
+}
