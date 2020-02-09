a4241c76a661c7a1deae23926192fcedcd88ec4d
evanrs <me@evanrs.com>
2020-02-09 02:50:38 -0500

    fix: missing types

--- tests/use-latest-version.spec.ts
+++ tests/use-latest-version.spec.ts
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { act, renderHook } from "@testing-library/react-hooks";
 import { DependencyList } from "react";
 
@@ -12,12 +13,18 @@ function setup<T>(initialValue: T, dependencies: DependencyList) {
   return renderHook(
     ({ defaultValue, dependencies }: Props<T>) =>
       useLatestVersion(defaultValue ?? initialValue, dependencies),
-    { initialProps: { defaultValue: initialValue, dependencies } }
+    {
+      initialProps: { defaultValue: initialValue, dependencies } as Props<T>
+    }
   );
 }
 
 function map<T>([latest, update]: [Value<T>, (v: T) => void]) {
   return { latest, update };
+}
+
+async function wait(ms?: number) {
+  await new Promise(next => setTimeout(next, ms ?? 10));
 }
 
 it("should return the default value", () => {
@@ -33,4 +40,25 @@ it("should update value", async () => {
   act(() => map(result.current).update(2));
 
   expect(map(result.current).latest.value).toBe(2);
+});
+
+it("should only accept highest version update", async () => {
+  const { result, rerender } = setup(1, ["first"]);
+
+  const first = map(result.current);
+
+  await wait();
+  rerender({ dependencies: ["second"] });
+  const second = map(result.current);
+
+  await wait();
+  rerender({ dependencies: ["third"] });
+  const third = map(result.current);
+
+  act(() => second.update(2));
+  act(() => first.update(1));
+  expect(map(result.current).latest.value).toBe(2);
+
+  act(() => third.update(3));
+  expect(map(result.current).latest.value).toBe(3);
 });
